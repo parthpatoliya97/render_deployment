4045ee138bb56c3a8427029d1236093f5edd28ae
parth patoliya <parthpatoliya001@gmail.com>
2025-03-25 11:19:17 +0530

    first commit

---
+++ src/routes/products/[name]/+page.server.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+
+export async function load({params}) {
+    const data = JSON.parse(fs.readFileSync(`content/products/${params.name}.json`, 'utf-8'));
+    
+
+    return  data
+        
+
+};
